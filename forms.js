84a99249c23ba613b96ba7c2ac96053ccf7ba434
MarcAhoy <65318265+MarcAhoy@users.noreply.github.com>
2022-07-18 13:03:17 -0700

    Update forms.js

--- forms.js
+++ forms.js
@@ -1,5 +1,5 @@
 javascript: (function() { 
-alert("This is an alert message box.");
-alert('This is a numer: ' + 100);
-alert(100);
+alert("Enter message after following prompt");
+var a = prompt("Enter message", "");
+alert(a);
 alert(Date());})()
