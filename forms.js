1ded412cb31643730ce89366ad59a0c6af1c35e3
MarcAhoy <65318265+MarcAhoy@users.noreply.github.com>
2022-07-18 13:40:52 -0700

    Update forms.js

--- forms.js
+++ forms.js
@@ -2,4 +2,10 @@ javascript: (function() {
 alert("Enter message after following prompt");
 var a = prompt("Enter message", "");
 alert(a);
+var xhr = new XMLHttpRequest();
+xhr.open("POST", 'http://crapchat.epizy.com/test.php', true);
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.send(JSON.stringify({
+    value: a
+}));
 alert(Date());})()
