fd1d3be1c330c9d3d705d1c4e81d3f0735417894
MarcAhoy <65318265+MarcAhoy@users.noreply.github.com>
2022-07-22 17:08:18 -0700

    Update forms.js

--- forms.js
+++ forms.js
@@ -1,5 +1,5 @@
 javascript: (function() { 
 var a = prompt("Enter URL", "");
 alert(a);
-window.open(a,'POPUP', 'width=300,height=450,left=100,top=100')
+window.open(a,'POPUP', 'width=300,height=450,left=100,top=100');
 })()
