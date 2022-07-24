3c775beca84ba4368dd9135e8278abfcb30065d7
MarcAhoy <65318265+MarcAhoy@users.noreply.github.com>
2022-07-23 20:47:54 -0700

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -4,3 +4,4 @@ var targetUrl = "http://afds.epizy.com/Proxy.php?=";
 window.open(targetUrl + dat);
 })()
 
+https://173-255-215-163.ip.linodeusercontent.com/site.php?url=https://stackoverflow.com/questions/39918819/how-to-enable-curl-in-nginx
