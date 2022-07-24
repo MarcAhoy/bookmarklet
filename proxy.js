e5dad5812bb16e7c63bcc9f13413d0614404fdb0
MarcAhoy <65318265+MarcAhoy@users.noreply.github.com>
2022-07-23 20:51:15 -0700

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -5,3 +5,5 @@ window.open(targetUrl + dat);
 })()
 
 https://173-255-215-163.ip.linodeusercontent.com/site.php?url=https://stackoverflow.com/questions/39918819/how-to-enable-curl-in-nginx
+
+https://173-255-215-163.ip.linodeusercontent.com/site.php?url=https://openvpn.com
